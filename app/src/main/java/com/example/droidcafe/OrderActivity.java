2c4e8d06c89099868fc217335ca9fe7d9e6ce27b
j-aguilar <3374485+j-aguilar@users.noreply.github.com>
2019-12-25 19:12:20 -0500

    add delivery options

--- app/src/main/java/com/example/droidcafe/OrderActivity.java
+++ app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -17,5 +20,30 @@ public class OrderActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textview = findViewById(R.id.order_tv);
         textview.setText(message);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()){
+            case R.id.sameday:
+                if (checked)
+                    displayToast(getString(R.string.same_day_message_service));
+                break;
+            case R.id.nextday:
+                if (checked)
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                if (checked)
+                    displayToast(getString(R.string.pick_up));
+                break;
+            default:
+                // Do nothing;
+                break;
+        }
+    }
+
+    public void displayToast (String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
